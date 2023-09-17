b7d0750795bd2dbf8b108ded5743720f7b3585c1
Crystian <crystian.lf@gmail.com>
2023-09-17 14:52:42 -0300

    Alterando o arquivo main

--- main.tf
+++ main.tf
@@ -13,13 +13,33 @@ provider "aws" {
   region = var.region
 }
 
+
+resource "aws_security_group" "alura-sg" {
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "django-app-instance" {
-  ami           = data.aws_ami.ubuntu.id
-  key_name      = var.key_name
-  instance_type = var.instance_type
+  ami             = data.aws_ami.ubuntu.id
+  key_name        = var.key_name
+  instance_type   = var.instance_type
+  count           = var.instances_count
+  security_groups = ["${aws_security_group.alura-sg.name}"]
 
   tags = {
-    Name = "Ansible-Terraform-Python Alura"
-    env  = "dev"
+    Name        = "alura_python_django"
+    Environment = "dev"
+    Stack       = "python"
   }
 }
