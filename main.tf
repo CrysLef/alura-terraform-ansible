ee8ea6523f3a88ac977f4d1c88d4f5466388669d
Crystian <crystian.lf@gmail.com>
2023-09-30 21:10:09 -0300

    Cria tabela para state lock

--- main.tf
+++ main.tf
@@ -31,3 +31,19 @@ resource "aws_instance" "django_app_instance" {
     Stack       = "python"
   }
 }
+
+resource "aws_dynamodb_table" "state_lock" {
+  name = "dynamodb-state-lock"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "dynamodb terraform state lock table"
+  }
+}
