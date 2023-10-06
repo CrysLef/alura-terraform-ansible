da1beb9742687c69a1884a29b5a507a55b77d21f
Crystian <crystian.lf@gmail.com>
2023-10-06 10:11:58 -0300

    Cria duas instancias separadas por stack

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,22 +18,22 @@ resource "aws_key_pair" "this" {
   public_key = var.public_key
 }
 
-resource "aws_instance" "django_app" {
+resource "aws_instance" "golang_api" {
   ami             = data.aws_ami.ubuntu.id
   key_name        = var.key_name
-  instance_type   = "${terraform.workspace == "prod" ? "t3.medium" : "t2.micro"}"
-  count           = "${terraform.workspace == "prod" ? 3 : 1}"
+  instance_type   = terraform.workspace == "prod" ? "t3.medium" : "t2.micro"
+  count           = terraform.workspace == "prod" ? 3 : 1
   security_groups = ["${aws_security_group.this.name}"]
 
-  tags = "${terraform.workspace == "prod" ? var.tag_prod : var.tag_teste}"
+  tags = terraform.workspace == "prod" ? var.tag_prod_golang : var.tag_teste_golang
 }
 
-resource "aws_instance" "django_app" {
+resource "aws_instance" "web_app" {
   ami             = data.aws_ami.ubuntu.id
   key_name        = var.key_name
-  instance_type   = "${terraform.workspace == "prod" ? "t3.medium" : "t2.micro"}"
-  count           = "${terraform.workspace == "prod" ? 3 : 1}"
+  instance_type   = terraform.workspace == "prod" ? "t3.medium" : "t2.micro"
+  count           = terraform.workspace == "prod" ? 3 : 1
   security_groups = ["${aws_security_group.this.name}"]
 
-  tags = "${terraform.workspace == "prod" ? var.tag_prod : var.tag_teste}"
+  tags = terraform.workspace == "prod" ? var.tag_prod_web : var.tag_teste_web
 }
